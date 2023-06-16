e02e868286a5cf4cb64d201fb35b5f132adf77a2
Simon Haïoun-Viet <simon12100@gmail.com>
2023-06-16 22:57:22 +0200

    feat(components): add About section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      typography: theme => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.400'),
+						'--tw-prose-bold': theme('colors.gray.300'),
+          },
+        },
+      }),
+    },
   },
-  plugins: [require('@tailwindcss/aspect-ratio')],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ],
 }
